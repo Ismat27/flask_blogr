dd95fbb5da26af8d8a5b45d79b26070408507026
unknown <adegbiteismail27@gmail.com>
2022-10-03 07:52:08 -0700

    test env

--- flaskr/__int__.py
+++ flaskr/__int__.py
@@ -1,10 +1,15 @@
+import os
 from flask import Flask, jsonify
+from dotenv import load_dotenv
+
+load_dotenv()
+message = os.environ.get('MESSAGE', 'I could not find any message')
 
 def create_app(test_config=None):
     app = Flask(__name__)
 
     @app.route('/')
     def index():
-        return jsonify({"Choo Choo": "Welcome to your Flask app"})
+        return jsonify({"message": message})
 
     return app
