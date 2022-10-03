7dcef04948590054c94a161417c2f7e5818a93d5
unknown <adegbiteismail27@gmail.com>
2022-10-03 08:54:14 -0700

    sample db

--- flaskr/models.py
+++ flaskr/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from sqlalchemy import Column, Text, String
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -8,3 +9,10 @@ def setup_db(app):
     db.app = app
     db.init_app(app)
     migrate.init_app(app, db)
+
+class Variable(db.Model):
+    __tablename__ = "variables"
+
+    name = Column(String(200), nullable=False)
+    value = Column(Text, nullable=False)
+
