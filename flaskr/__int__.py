1e873a332ab460fc015cc4fbdcfbc8d91f6bb022
unknown <adegbiteismail27@gmail.com>
2022-10-03 08:41:58 -0700

    initiate configuration

--- flaskr/__int__.py
+++ flaskr/__int__.py
@@ -1,12 +1,16 @@
 import os
 from flask import Flask, jsonify
 from dotenv import load_dotenv
+from config import DevelopmentConfig
+from models import setup_db
 
 load_dotenv()
 message = os.environ.get('MESSAGE', 'I could not find any message')
 
-def create_app(test_config=None):
+def create_app(config=DevelopmentConfig):
     app = Flask(__name__)
+    app.config.from_object(config)
+    setup_db(app)
 
     @app.route('/')
     def index():
